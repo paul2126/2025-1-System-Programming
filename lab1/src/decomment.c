8aa907fd068b32d011e6cf3b9230ec5b4cfcc1b9
paul2126 <paul.mhryu@gmail.com>
2025-03-18 02:32:15 +0900

    format source code

--- lab1/src/decomment.c
+++ lab1/src/decomment.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
+// RyuMyungHyun
+// assignmnet1
+// decomment.c
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-/* This is skeleton code for reading characters from 
-standard input (e.g., a file or console input) one by one until 
-the end of the file (EOF) is reached. It keeps track of the current 
-line number and is designed to be extended with additional 
-functionality, such as processing or transforming the input data. 
-In this specific task, the goal is to implement logic that removes 
+/* This is skeleton code for reading characters from
+standard input (e.g., a file or console input) one by one until
+the end of the file (EOF) is reached. It keeps track of the current
+line number and is designed to be extended with additional
+functionality, such as processing or transforming the input data.
+In this specific task, the goal is to implement logic that removes
 C-style comments from the input. */
 
-int main(void)
-{
-  // ich: int type variable to store character input from getchar() (abbreviation of int character)
+int main(void) {
+  // ich: int type variable to store character input from getchar()
+  // (abbreviation of int character)
   int ich;
-  // line_cur & line_com: current line number and comment line number (abbreviation of current line and comment line)
+  // line_cur & line_com: current line number and comment line number
+  // (abbreviation of current line and comment line)
   int line_cur, line_com;
-  // ch: character that comes from casting (char) on ich (abbreviation of character)
+  // ch: character that comes from casting (char) on ich (abbreviation
+  // of character)
   char ch;
 
   line_cur = 1;
@@ -27,7 +32,7 @@ int main(void)
     int got_eof = 0;
 
     ich = getchar();
-    if (ich == EOF) 
+    if (ich == EOF)
       break;
 
     ch = (char)ich;
@@ -38,6 +43,6 @@ int main(void)
     if (got_eof)
       break;
   }
-  
-  return(EXIT_SUCCESS);
+
+  return (EXIT_SUCCESS);
 }
