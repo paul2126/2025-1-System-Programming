6fdc730fb50a08617f1cb13ab40fce42a346456a
paul2126 <paul.mhryu@gmail.com>
2025-04-07 12:00:22 +0900

    lint file

--- lab2/src/dirtree.c
+++ lab2/src/dirtree.c
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// System Programming                         I/O Lab                                    Spring 2025
+// System Programming                         I/O Lab Spring 2025
 //
 /// @file
 /// @brief resursively traverse directory tree and list all entries
@@ -8,92 +8,94 @@
 //--------------------------------------------------------------------------------------------------
 
 #define _GNU_SOURCE
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
 #include <unistd.h>
-#include <stdarg.h>
-#include <assert.h>
-#include <grp.h>
-#include <pwd.h>
 
-#define MAX_DIR 64            ///< maximum number of supported directories
+#define MAX_DIR 64 ///< maximum number of supported directories
 
 /// @brief output control flags
-#define F_TREE      0x1       ///< enable tree view
-#define F_SUMMARY   0x2       ///< enable summary
-#define F_VERBOSE   0x4       ///< turn on verbose mode
+#define F_TREE 0x1    ///< enable tree view
+#define F_SUMMARY 0x2 ///< enable summary
+#define F_VERBOSE 0x4 ///< turn on verbose mode
 
 /// @brief struct holding the summary
 struct summary {
-  unsigned int dirs;          ///< number of directories encountered
-  unsigned int files;         ///< number of files
-  unsigned int links;         ///< number of links
-  unsigned int fifos;         ///< number of pipes
-  unsigned int socks;         ///< number of sockets
+  unsigned int dirs;  ///< number of directories encountered
+  unsigned int files; ///< number of files
+  unsigned int links; ///< number of links
+  unsigned int fifos; ///< number of pipes
+  unsigned int socks; ///< number of sockets
 
-  unsigned long long size;    ///< total size (in bytes)
-  unsigned long long blocks;  ///< total number of blocks (512 byte blocks)
+  unsigned long long size; ///< total size (in bytes)
+  unsigned long long
+      blocks; ///< total number of blocks (512 byte blocks)
 };
 
-
-/// @brief abort the program with EXIT_FAILURE and an optional error message
+/// @brief abort the program with EXIT_FAILURE and an optional error
+/// message
 ///
 /// @param msg optional error message or NULL
-void panic(const char *msg)
-{
-  if (msg) fprintf(stderr, "%s\n", msg);
+void panic(const char *msg) {
+  if (msg)
+    fprintf(stderr, "%s\n", msg);
   exit(EXIT_FAILURE);
 }
 
-
-/// @brief read next directory entry from open directory 'dir'. Ignores '.' and '..' entries
+/// @brief read next directory entry from open directory 'dir'. Ignores
+/// '.' and '..' entries
 ///
 /// @param dir open DIR* stream
 /// @retval entry on success
 /// @retval NULL on error or if there are no more entries
-struct dirent *getNext(DIR *dir)
-{
+struct dirent *getNext(DIR *dir) {
   struct dirent *next;
   int ignore;
 
   do {
     errno = 0;
     next = readdir(dir);
-    if (errno != 0) perror(NULL);
-    ignore = next && ((strcmp(next->d_name, ".") == 0) || (strcmp(next->d_name, "..") == 0));
+    if (errno != 0)
+      perror(NULL);
+    ignore = next && ((strcmp(next->d_name, ".") == 0) ||
+                      (strcmp(next->d_name, "..") == 0));
   } while (next && ignore);
 
   return next;
 }
 
-
-/// @brief qsort comparator to sort directory entries. Sorted by name, directories first.
+/// @brief qsort comparator to sort directory entries. Sorted by name,
+/// directories first.
 ///
 /// @param a pointer to first entry
 /// @param b pointer to second entry
 /// @retval -1 if a<b
 /// @retval 0  if a==b
 /// @retval 1  if a>b
-static int dirent_compare(const void *a, const void *b)
-{
-  struct dirent *e1 = (struct dirent*)a;
-  struct dirent *e2 = (struct dirent*)b;
+static int dirent_compare(const void *a, const void *b) {
+  struct dirent *e1 = (struct dirent *)a;
+  struct dirent *e2 = (struct dirent *)b;
 
   // if one of the entries is a directory, it comes first
   if (e1->d_type != e2->d_type) {
-    if (e1->d_type == DT_DIR) return -1;
-    if (e2->d_type == DT_DIR) return 1;
+    if (e1->d_type == DT_DIR)
+      return -1;
+    if (e2->d_type == DT_DIR)
+      return 1;
   }
 
   // otherwise sorty by name
   return strcmp(e1->d_name, e2->d_name);
 }
-
 
 /// @brief recursively process directory @a dn and print its tree
 ///
@@ -101,19 +103,18 @@ static int dirent_compare(const void *a, const void *b)
 /// @param pstr prefix string printed in front of each entry
 /// @param stats pointer to statistics
 /// @param flags output control flags (F_*)
-void processDir(const char *dn, const char *pstr, struct summary *stats, unsigned int flags)
-{
+void processDir(const char *dn, const char *pstr, struct summary *stats,
+                unsigned int flags) {
   // TODO
 }
 
-
-/// @brief print program syntax and an optional error message. Aborts the program with EXIT_FAILURE
+/// @brief print program syntax and an optional error message. Aborts
+/// the program with EXIT_FAILURE
 ///
 /// @param argv0 command line argument 0 (executable)
 /// @param error optional error (format) string (printf format) or NULL
 /// @param ... parameter to the error format string
-void syntax(const char *argv0, const char *error, ...)
-{
+void syntax(const char *argv0, const char *error, ...) {
   if (error) {
     va_list ap;
 
@@ -126,31 +127,35 @@ void syntax(const char *argv0, const char *error, ...)
 
   assert(argv0 != NULL);
 
-  fprintf(stderr, "Usage %s [-t] [-s] [-v] [-h] [path...]\n"
-                  "Gather information about directory trees. If no path is given, the current directory\n"
-                  "is analyzed.\n"
-                  "\n"
-                  "Options:\n"
-                  " -t        print the directory tree (default if no other option specified)\n"
-                  " -s        print summary of directories (total number of files, total file size, etc)\n"
-                  " -v        print detailed information for each file. Turns on tree view.\n"
-                  " -h        print this help\n"
-                  " path...   list of space-separated paths (max %d). Default is the current directory.\n",
-                  basename(argv0), MAX_DIR);
+  fprintf(stderr,
+          "Usage %s [-t] [-s] [-v] [-h] [path...]\n"
+          "Gather information about directory trees. If no path is "
+          "given, the current directory\n"
+          "is analyzed.\n"
+          "\n"
+          "Options:\n"
+          " -t        print the directory tree (default if no other "
+          "option specified)\n"
+          " -s        print summary of directories (total number of "
+          "files, total file size, etc)\n"
+          " -v        print detailed information for each file. Turns "
+          "on tree view.\n"
+          " -h        print this help\n"
+          " path...   list of space-separated paths (max %d). Default "
+          "is the current directory.\n",
+          basename(argv0), MAX_DIR);
 
   exit(EXIT_FAILURE);
 }
 
-
 /// @brief program entry point
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   //
   // default directory is the current directory (".")
   //
   const char CURDIR[] = ".";
   const char *directories[MAX_DIR];
-  int   ndir = 0;
+  int ndir = 0;
 
   struct summary tstat;
   unsigned int flags = 0;
@@ -161,24 +166,31 @@ int main(int argc, char *argv[])
   for (int i = 1; i < argc; i++) {
     if (argv[i][0] == '-') {
       // format: "-<flag>"
-      if      (!strcmp(argv[i], "-t")) flags |= F_TREE;
-      else if (!strcmp(argv[i], "-s")) flags |= F_SUMMARY;
-      else if (!strcmp(argv[i], "-v")) flags |= F_VERBOSE;
-      else if (!strcmp(argv[i], "-h")) syntax(argv[0], NULL);
-      else syntax(argv[0], "Unrecognized option '%s'.", argv[i]);
+      if (!strcmp(argv[i], "-t"))
+        flags |= F_TREE;
+      else if (!strcmp(argv[i], "-s"))
+        flags |= F_SUMMARY;
+      else if (!strcmp(argv[i], "-v"))
+        flags |= F_VERBOSE;
+      else if (!strcmp(argv[i], "-h"))
+        syntax(argv[0], NULL);
+      else
+        syntax(argv[0], "Unrecognized option '%s'.", argv[i]);
     } else {
       // anything else is recognized as a directory
       if (ndir < MAX_DIR) {
         directories[ndir++] = argv[i];
       } else {
-        printf("Warning: maximum number of directories exceeded, ignoring '%s'.\n", argv[i]);
+        printf("Warning: maximum number of directories exceeded, "
+               "ignoring '%s'.\n",
+               argv[i]);
       }
     }
   }
 
   // if no directory was specified, use the current directory
-  if (ndir == 0) directories[ndir++] = CURDIR;
-
+  if (ndir == 0)
+    directories[ndir++] = CURDIR;
 
   //
   // process each directory
@@ -187,7 +199,8 @@ int main(int argc, char *argv[])
   //
   // Pseudo-code
   // - reset statistics (tstat)
-  // - loop over all entries in 'directories' (number of entires stored in 'ndir')
+  // - loop over all entries in 'directories' (number of entires stored
+  // in 'ndir')
   //   - reset statistics (dstat)
   //   - if F_SUMMARY flag set: print header
   //   - print directory name
@@ -195,7 +208,6 @@ int main(int argc, char *argv[])
   //   - if F_SUMMARY flag set: print summary & update statistics
   memset(&tstat, 0, sizeof(tstat));
   //...
-
 
   //
   // print grand total
@@ -207,14 +219,14 @@ int main(int argc, char *argv[])
            "  total # of links:        %16d\n"
            "  total # of pipes:        %16d\n"
            "  total # of sockets:      %16d\n",
-           ndir, tstat.files, tstat.dirs, tstat.links, tstat.fifos, tstat.socks);
+           ndir, tstat.files, tstat.dirs, tstat.links, tstat.fifos,
+           tstat.socks);
 
     if (flags & F_VERBOSE) {
       printf("  total file size:         %16llu\n"
              "  total # of blocks:       %16llu\n",
              tstat.size, tstat.blocks);
     }
-
   }
 
   //
@@ -222,4 +234,3 @@ int main(int argc, char *argv[])
   //
   return EXIT_SUCCESS;
 }
-
