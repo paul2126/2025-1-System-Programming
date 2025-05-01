64ccc10dd97e53b8cf4b3f0c3437717274c58074
paul2126 <paul.mhryu@gmail.com>
2025-05-01 12:27:44 +0900

    feat(src/chunk.h): add function for double list

--- lab3/src/chunk.h
+++ lab3/src/chunk.h
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------*/
-/* This file is almost empty, and is provided to enable you to use    */ 
+/* This file is almost empty, and is provided to enable you to use    */
 /* the Makefile.                                                      */
 /* You are free to modify this file if you want.                      */
 /* Even if you do not use this file, please keep it for the Makefile  */
@@ -14,8 +14,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-
-/* 
+/*
    each chunk has a header and data units. each header in a chunk has
    a pointer to the next chunk in the free list. the size of a chunk
    is in the number of units, but it does not include the unit for a
@@ -26,58 +25,59 @@
 typedef struct Chunk *Chunk_T;
 
 enum {
-   CHUNK_FREE,
-   CHUNK_IN_USE,
+  CHUNK_FREE,
+  CHUNK_IN_USE,
 };
 
 enum {
-   CHUNK_UNIT = 16,        /* 16 = sizeof(struct Chunk) */
+  CHUNK_UNIT = 24, /* 24 = sizeof(struct Chunk) */
 };
 
 /* chunk_get_status:
- * Returns a chunk's status which shows whether the chunk is in use or free.
- * Return value is either CHUNK_IN_USE or CHUNK_FREE. */
-int
-chunk_get_status(Chunk_T c);
+ * Returns a chunk's status which shows whether the chunk is in use or
+ * free. Return value is either CHUNK_IN_USE or CHUNK_FREE. */
+int chunk_get_status(Chunk_T c);
 
 /* chunk_set_status:
- * Set the status of the chunk, 'c'. 
+ * Set the status of the chunk, 'c'.
  * status can be either CHUNK_FREE or CHUNK_IN_USE */
-void
-chunk_set_status(Chunk_T c, int status);
+void chunk_set_status(Chunk_T c, int status);
 
 /* chunk_get_units:
- * Returns the size of a chunk, 'c', in terms of the number of chunk units. */
-int
-chunk_get_units(Chunk_T c);
+ * Returns the size of a chunk, 'c', in terms of the number of chunk
+ * units. */
+int chunk_get_units(Chunk_T c);
 
 /* chunk_set_units:
  * Sets the current size in 'units' of 'c' */
-void
-chunk_set_units(Chunk_T c, int units);
+void chunk_set_units(Chunk_T c, int units);
 
 /* chunk_get_next_free_chunk:
- * Returns the next free chunk in free chunk list.
- * Returns NULL if 'c' is the last free chunk in the list. */
-Chunk_T
-chunk_get_next_free_chunk(Chunk_T c);
+ * Returns the next free chunk in free chunk list. */
+Chunk_T chunk_get_next_free_chunk(Chunk_T c);
 
 /* chunk_set_next_free_chunk:
  * Sets the next free chunk of 'c' to 'next') */
-void
-chunk_set_next_free_chunk(Chunk_T c, Chunk_T next);
+void chunk_set_next_free_chunk(Chunk_T c, Chunk_T next);
+
+/* chunk_get_prev_free_chunk:
+ * Returns the previous free chunk in free chunk list.*/
+Chunk_T chunk_get_prev_free_chunk(Chunk_T c);
+
+/* chunk_set_prev_free_chunk:
+ * Sets the previous free chunk of 'c' to 'prev') */
+void chunk_set_prev_free_chunk(Chunk_T c, Chunk_T prev);
 
 /* chunk_get_next_adjacent:
  * Returns the next adjacent chunk to 'c' in memory space.
  * start is the pointer to the start of the Heap
  * end is the the pointer to the end of the Heap
  * Returns NULL if 'c' is the last chunk in memory space. */
-Chunk_T
-chunk_get_next_adjacent(Chunk_T c, void *start, void *end);
+Chunk_T chunk_get_next_adjacent(Chunk_T c, void *start, void *end);
 
 /* Following two functions are for debugging.
  * These will be removed by C preprocessor if you compile the code with
- * -DNDEBUG option. 
+ * -DNDEBUG option.
  */
 #ifndef NDEBUG
 /* chunk_is_valid:
@@ -85,8 +85,7 @@ chunk_get_next_adjacent(Chunk_T c, void *start, void *end);
  * start is the pointer to the start of the Heap
  * end is the the pointer to the end of the Heap
  */
-int 
-chunk_is_valid(Chunk_T c, void *start, void *end);
+int chunk_is_valid(Chunk_T c, void *start, void *end);
 
 #endif /* NDEBUG */
 
